ca47aed12a8ca94093492a6ec37674ff9dd807ce
Moses <agunbiademoses2009@gmail.com>
2025-12-28 16:17:32 +0000

    added quiz question to Questions.js

---
+++ src/Components/Questions.js
@@ -0,0 +1,80 @@
+data =
+[
+{
+question: "When Mary became pregnant, Mary and Joseph were:",
+option1: "Married",
+option2: "Engaged",
+option3: "Just friends",
+option4: "None of the above",
+ans: 2
+},
+{
+question: "When Mary became pregnant",
+option1: "Joseph married her", 
+option2: "Joseph wanted to dissolve their relationship",
+option3: "Mary left Nazareth for a while", 
+option4: "An angel told them to go to Bethlehem",
+ans: 2
+},
+{
+question: "Who directed Mary and Joseph to go to Bethlehem?",
+option1: "Herod", 
+option2: "Caesar",
+option3: "An angel",
+option4: "The IRS",
+ans: 2 
+},
+{
+question: "Joseph’s family was originally from",
+option1: "Jerusalem",
+option2: "Bethlehem",
+option3: "Nazareth",
+option4: "None of the above",
+Answer: 2 
+},
+
+{
+question: "For the journey to Bethlehem Mary and Joseph",
+option1: "walked",
+option2: "Joseph walked and Mary rode a donkey",
+option3: "Took a bus",
+option4: "The Bible does not say",
+ans: 4 
+},
+{
+question: "Who told Joseph to name the baby Jesus?",
+option1: "Mary", 
+option2: "The chief priests and scribes",
+option3: "An angel of the Lord",
+option4: "Herod the king",
+ans: 3 
+},
+{
+question: "Jesus Christ is the Son of God",
+option1: "True",
+option2: "False",
+ans: 1
+},
+{
+question: "What animals were present at Jesus birth?",
+option1: "Cows, sheep and camels",
+option2: "Cows, chickens and donkeys",
+option3: "ions, tigers, and bears",
+option4: "The Bible does not say",
+ans: 4 
+},
+
+{
+question: "when did baby Jesus cry?",
+option1: "when he saw the wise men",
+option2: "whenever babies usually cry",
+option3: "when the cattle started lowing",
+option4: "No crying he makes",
+ans: 2 
+},
+{
+question: "Why was Jesus Christ born?",
+
+ans: "To save people from sin"
+}
+]
