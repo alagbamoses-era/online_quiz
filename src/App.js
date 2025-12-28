d199cbe09702e2ef6637266c14bc461378814408
Moses <agunbiademoses2009@gmail.com>
2025-12-28 17:05:26 +0000

    css added to question page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import { Quiz } from './Components/Quiz';
 
 import './App.css';
 
 function App() {
   return (
-    <div>I am a programmer</div>
+    <Quiz />
   );
 }
 
--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import '../css/Quiz.css';
 
 export const Quiz = () => {
 
     return (
-        <div>
-            <div>
+        <div className='container'>
+            <div className='question-app'>
             <h1>Quiz App</h1>
+            <hr />
             <h2>1. When Mary became pregnant, Mary and Joseph were:</h2>
             <ul>
                 <li>Married</li>
@@ -13,7 +15,7 @@ export const Quiz = () => {
                 <li>Just friends</li>
                 <li>None of the above</li>
             </ul>
-            <h2>1 of 10 questions</h2>
+            <h2 className='index'>1 of 10 questions</h2>
             <button>Next</button>
             </div>
         </div>
