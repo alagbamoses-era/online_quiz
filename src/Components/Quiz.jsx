d0d29b40ed4882593d50df2e7ab516674ee5adc6
Moses <agunbiademoses2009@gmail.com>
2025-12-28 16:27:34 +0000

    added Quiz question template to Quiz.jsx

---
+++ src/Components/Quiz.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Quiz = () => {
+
+    return (
+        <div>
+            <div>
+            <h1>Quiz App</h1>
+            <h2>1. When Mary became pregnant, Mary and Joseph were:</h2>
+            <ul>
+                <li>Married</li>
+                <li>Engaged</li>
+                <li>Just friends</li>
+                <li>None of the above</li>
+            </ul>
+            <h2>1 of 10 questions</h2>
+            <button>Next</button>
+            </div>
+        </div>
+    )
+}
