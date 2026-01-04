b3946c00f8870e880eda4382139d0375e20d119b
Moses <agunbiademoses2009@gmail.com>
2026-01-04 15:44:46 +0000

    added logic to identify right and wrong answer

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -5,8 +5,17 @@ import NavBar from './NavBar';
 
 export const Quiz = () => {
 
-    const [index, setIndex] = useState(0);
+    const [index, setIndex] = useState(1);
     const [question, setQuestion] = useState(data[index])
+
+    const checkAns = (e, q_ans) => {
+        if (question.ans === q_ans) {
+            e.target.classList.add("correct")
+
+        } else {
+            e.target.classList.add("wrong")
+        }
+    }
 
     return (
         <>
@@ -17,10 +26,10 @@ export const Quiz = () => {
             <hr />
             <h2>{index + 1}. {question.question}</h2>
             <ul>
-                <li>{question.option1}</li>
-                <li>{question.option2}</li>
-                <li>{question.option3}</li>
-                <li>{question.option4}</li>
+                <li onClick={((e) => {checkAns(e, 1)})}>{question.option1}</li>
+                <li onClick={((e) => {checkAns(e, 2)})}>{question.option2}</li>
+                <li onClick={((e) => {checkAns(e, 3)})}>{question.option3}</li>
+                <li onClick={((e) => {checkAns(e, 4)})}>{question.option4}</li>
             </ul>
             <h2 className='index'>{index +1} of {data.length} questions</h2>
             <button>Next</button>
